ce78c099f872c6db9911ef0d3cf03be572ac823a
guanshan <guanshan007@outlook.com>
2020-10-18 17:33:44 +0800

    add a index page

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 app = Flask(__name__)
 
-# 视图函数（view funciton），你可以理解为“请求处理函数”
+# 视图函数(view funciton)--“请求处理函数”
 # 只需要写出相对地址，主机地址、端口号等都不需要写出
 # http://localhost:5000/
 @app.route("/") 
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    return render_template('index.html',
+    name = name,
+    movies = movies)
