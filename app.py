bcbb34a07a9e1d506b9f4b1407994c06fad6ff7e
guanshan <guanshan007@outlook.com>
2020-10-18 23:26:11 +0800

    add database support with Flask-SQLAlchemy, but flask shell/flask forge??

--- app.py
+++ app.py
@@ -1,27 +1,64 @@
+import os
+import sys
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy 
+
+
+WIN = sys.platform.startswith('win')
+if WIN:
+    # windows sqlite 系统使用三斜线
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
+    os.path.join(app.root_path, 'data.db')
+# 关闭对模型修改的监控 （？？？）
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 
 name = 'Grey Li'
 movies = [
-    {'title': 'My Neighbor Totoro', 'year': '1988'},
-    {'title': 'Dead Poets Society', 'year': '1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
 ]
 
-app = Flask(__name__)
+user = User(name=name)
+db.session.add(user)
+for m in movies:
+    movie = Movie(title=m['title'], year=m['year'])
+    db.session.add(movie)
+
+db.session.commit()
 
 # 视图函数(view funciton)--“请求处理函数”
 # 只需要写出相对地址，主机地址、端口号等都不需要写出
 # http://localhost:5000/
 @app.route("/") 
 def index():
+    user = User.query.first()
+    print(user)
+    movies = Movie.query.all()
     return render_template('index.html',
-    name = name,
+    user = user,
     movies = movies)
