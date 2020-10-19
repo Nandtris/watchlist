44e3df4e5f08abbddb85ced3b870f5c3fa184a90
guanshan <guanshan007@outlook.com>
2020-10-19 19:26:08 +0800

    add base and error templates

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
+import click
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy 
 
@@ -13,10 +14,10 @@ else:
     prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + \
-    os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 # 关闭对模型修改的监控 （？？？）
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -29,36 +30,56 @@ class Movie(db.Model):
     year = db.Column(db.String(4))
 
 
-name = 'Grey Li'
-movies = [
-{'title': 'My Neighbor Totoro', 'year': '1988'},
-{'title': 'Dead Poets Society', 'year': '1989'},
-{'title': 'A Perfect World', 'year': '1993'},
-{'title': 'Leon', 'year': '1994'},
-{'title': 'Mahjong', 'year': '1996'},
-{'title': 'Swallowtail Butterfly', 'year': '1996'},
-{'title': 'King of Comedy', 'year': '1999'},
-{'title': 'Devils on the Doorstep', 'year': '1999'},
-{'title': 'WALL-E', 'year': '2008'},
-{'title': 'The Pork of Music', 'year': '2012'},
-]
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database"""
+    if drop: # flask initdb --drop
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
 
-user = User(name=name)
-db.session.add(user)
-for m in movies:
-    movie = Movie(title=m['title'], year=m['year'])
-    db.session.add(movie)
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
 
-db.session.commit()
+    name = 'Grey Li'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},]
+    
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done')
+
+# 模板上下文处理函数
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 # 视图函数(view funciton)--“请求处理函数”
 # 只需要写出相对地址，主机地址、端口号等都不需要写出
 # http://localhost:5000/
 @app.route("/") 
 def index():
-    user = User.query.first()
-    print(user)
+    # user = User.query.first()
+    # print(user)
     movies = Movie.query.all()
-    return render_template('index.html',
-    user = user,
-    movies = movies)
+    return render_template('index.html', movies = movies)
